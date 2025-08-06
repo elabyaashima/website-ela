638aff57c13dca8740facc30c90b49a041a5a637
whyrupesh <srupeshkumar800@gmail.com>
2025-08-06 14:13:16 +0530

    checking

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
             <p className='font-medium text-sm md:text-base'>OUR BESTSELLERS</p>
           </div>
           <h1 className='prata-regular text-3xl sm:py-3 lg:text-5xl leading-relaxed'>
-            Latest Arrivals
+            Latest Exclusives
           </h1>
           <div className='flex items-center gap-2'>
             <p className='font-semibold text-sm md:text-base'>SHOP NOW</p>
